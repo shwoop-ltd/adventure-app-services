156db8a33853a59b838d698d8560b09b163d2c54
Nevera <russellbloxwich@gmail.com>
2019-07-02 16:17:09 +1200

    Conflict resolution

--- lambda/functions/finish-challenge.ts
+++ lambda/functions/finish-challenge.ts
@@ -2,7 +2,7 @@ import { DynamoDB } from 'aws-sdk';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 
 const prizes_table_name = process.env.PRIZES_TABLE_NAME!;
-const doc_client = new DynamoDB.DocumentClient({ region: process.env.REGION, endpoint: process.env.ENDPOINT_OVERRIDE || undefined });
+const doc_client = new DynamoDB.DocumentClient({ region: process.env.REGION, endpoint: process.env.ENDPOINT_OVERRIDE });
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   // TODO: Get info about user, store user info
