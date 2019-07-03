53c321ffbc31e77597e0c7f2f001759e2d5aabf1
Timothy Finucane <timfinucane@outlook.com>
2019-07-03 14:54:17 +1200

    Use template include to fill in api gateway integration values, and preupload openapi to do so

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,13 @@
-sam validate --profile shwoop
+sam validate
 
-sam package --template-file template.yaml --s3-bucket shwoop-cloudformation --output-template-file cloudformation.yaml --profile shwoop
-sam deploy --template-file ./cloudformation.yaml --stack-name shwoop --capabilities CAPABILITY_IAM --profile shwoop
+# Compile code
+# TODO: Should be able to run tsc from root to compile
+cd lambda
+tsc
+cd ..
+
+# Pre-upload the openapi.yaml so it is accessible for sam
+aws s3 cp ./api-gateway/openapi.yaml s3://adventure-app-cloudformation/
+
+sam package --template-file template.yaml --s3-bucket adventure-app-cloudformation --output-template-file cloudformation.yaml
+sam deploy --template-file ./cloudformation.yaml --stack-name shwoop --capabilities CAPABILITY_IAM
