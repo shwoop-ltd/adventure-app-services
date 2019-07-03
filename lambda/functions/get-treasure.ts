1dd4c436b482b83977c0fc476ee108fbd7d7d633
Timothy Finucane <timfinucane@outlook.com>
2019-07-03 16:20:02 +1200

    Fix error in deployed get-treasure

--- lambda/functions/get-treasure.ts
+++ lambda/functions/get-treasure.ts
@@ -4,7 +4,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 const table_name = process.env.TABLE_NAME!;
 const accuracy = 0.00005 //TODO: Move this to environment variables
 const prizes_table_name = process.env.PRIZES_TABLE_NAME!;
-const doc_client = new DynamoDB.DocumentClient({ region: process.env.REGION, endpoint: process.env.ENDPOINT_OVERRIDE });
+const doc_client = new DynamoDB.DocumentClient({ region: process.env.REGION, endpoint: process.env.ENDPOINT_OVERRIDE || undefined });
 
 function generateRandomString(length: number) { //TODO: Put this in its own class.
     let returnString = ""
