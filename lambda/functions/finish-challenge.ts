9820c26092c1c5864d58df76feac084de8b4f5bd
Russell Bloxwich <your_email@whatever.com>
2019-07-06 13:30:52 +1200

    Finish-challenge now gets prize from puzzle

--- lambda/functions/finish-challenge.ts
+++ lambda/functions/finish-challenge.ts
@@ -1,6 +1,8 @@
 import { DynamoDB } from 'aws-sdk';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 
+const table_name = process.env.TABLE_NAME!;
+const users_table_name = process.env.USERS_TABLE_NAME!;
 const prizes_table_name = process.env.PRIZES_TABLE_NAME!;
 const doc_client = new DynamoDB.DocumentClient({ region: process.env.REGION, endpoint: process.env.ENDPOINT_OVERRIDE || undefined });
 
@@ -14,39 +16,81 @@ function generateRandomString(length: number) {
 }
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-  // TODO: Get info about user, store user info
-
-  if (!event.pathParameters)
-    return { statusCode: 400, body: "No path parameters" };
-  if (!event.queryStringParameters || !event.queryStringParameters.challenge)
-    return { statusCode: 400, body: "Challenge ID was not present" };
+  //Request contents check
   if (!event.body) {
-    return { statusCode: 400, body: "Solution beacon not present" }
+    return { statusCode: 400, body: "Body not present" }
+  }
+  const body = JSON.parse(event.body);
+  if (!body.challenge_id || !body.beacon_id || !body.map) {
+    return { statusCode: 400, body: "Incorrect body." }
   }
 
-  const solution = JSON.parse(event.body).beacon_id;
-  const key = event.queryStringParameters.challenge;
+  //Core variable assignment
+  const solution = body.beacon_id;
+  const challenge_id = body.challenge_id;
+  const map = body.map;
 
-  const result = await doc_client.get({ TableName: prizes_table_name, Key: { id: key } }).promise();
-
-  if (!result.Item) {
-    return { statusCode: 404, body: `There is no challenge with an id of ${key}` };
+  //User Check
+  const user_id = event.headers.Authorization.substring(7);
+  const user_result = await doc_client.get({ TableName: users_table_name, Key: { "id": user_id } }).promise();
+  const user = user_result.Item;
+  if (!user) {
+    return { statusCode: 401, body: "User does not exist." }
   }
 
-  if (result.Item.solution === solution) {// Correct, they get a prize. TODO: Actually get prizes from the puzzle
-    var d = new Date();
-    let prize = {
-      id: generateRandomString(8),
-      type: "red-bull",
-      received: d.toISOString(),
-      received_from: "Challenge",
-      claimed: false
-    };
-    doc_client.put({ TableName: prizes_table_name, Item: prize }, function (err, data) { });
-    //The callback is neccesary for this function to work.
-
-    return { statusCode: 200, body: JSON.stringify(prize) };
+  //Prize Check
+  const puzzle_key = "puzzle-" + map + "-beacon-" + challenge_id;
+  const puzzle_result = await doc_client.get({ TableName: table_name, Key: { "id": puzzle_key } }).promise();
+  if (!puzzle_result.Item) {
+    return { statusCode: 502, body: "Something went wrong!" }
   }
-  else // Not correct, so no prize
-    return { statusCode: 204, body: `Beacon ${solution} is not the correct answer for this challenge!` };
+  const puzzle = puzzle_result.Item;
+
+  //Solution Check
+  if (!puzzle.solution === solution) {
+    return { statusCode: 400, body: "Wrong solution." }
+  };
+
+  //Everything is good, lets get this man (or woman) a prize.
+
+  //Establish prize object
+  const d = new Date();
+  const prize = {
+    id: generateRandomString(8),
+    type: "red-bull",
+    received: d.toISOString(),
+    received_from: "Challenge",
+    claimed: false,
+    points: undefined
+  };
+
+  //Identify the prize that should be awarded.
+  let total = 0;
+  for (let i = 0; i < puzzle.prizes.length; i++) {
+    total += puzzle.prizes[i].available;
+    if (puzzle.claimed < total) {
+      prize.type = puzzle.prizes[i].prize;
+      break;
+    }
+  };
+
+  //Set up updating user's prizes
+  user.prizes.push(prize.id);
+  var prizes_params = {
+    TableName: users_table_name,
+    Key: { "id": user_id },
+    UpdateExpression: 'SET prizes = :x',
+    ExpressionAttributeValues: {
+      ':x': user.prizes
+    }
+  };
+
+  //Update awarded prizes
+  doc_client.update(prizes_params, function (err, data) { if (err) return { statusCode: 418, body: err } });
+
+  //Store prize in prize table
+  doc_client.put({ TableName: prizes_table_name, Item: prize }, function (err, data) { });
+
+  //Return the prize
+  return { statusCode: 200, body: JSON.stringify(prize) };
 }
