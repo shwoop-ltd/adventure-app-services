9ed1907128c9b3e35b5e23a49361d71ce54a3c40
Timothy Finucane <timfinucane@outlook.com>
2019-08-12 18:53:42 +1200

    Simplify deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,7 @@
 sam validate
 
 # Compile code
-# TODO: Should be able to run tsc from root to compile
-cd lambda
-tsc
-cd ..
+tsc -p .\lambda
 
 sam package --template-file template.yaml --s3-bucket adventure-app-cloudformation --output-template-file cloudformation.yaml
 sam deploy --template-file ./cloudformation.yaml --stack-name adventure-app --capabilities CAPABILITY_IAM
