15b511c06a7cd26471ec35949b5427bb8fb08e0d
Timothy Finucane <timfinucane@outlook.com>
2019-08-16 01:07:26 +1200

    Update scripts to work on desktop. Scripts now run correctly

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-sam validate "${@:2}"
-
-# Compile code
-tsc
-
-sam package --template-file template.yaml --s3-bucket adventure-app-cloudformation --s3-prefix $1 --output-template-file cloudformation.yaml "${@:2}"
-sam deploy --template-file ./cloudformation.yaml --stack-name AdventureApp$1 --capabilities CAPABILITY_IAM "${@:2}"
