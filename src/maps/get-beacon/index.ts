a5cfcf8f62bc7dc0603a0c502eebfed3d76faf51
Timothy Finucane <timfinucane@outlook.com>
2019-08-21 15:24:26 +1200

    Minor fixes

--- src/maps/get-beacon/index.ts
+++ src/maps/get-beacon/index.ts
@@ -8,14 +8,13 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
   if(!event.pathParameters)
     return { statusCode: 400, body: "No path parameters" };
 
-  const map_name = event.pathParameters.map;
-  const beacon_id = event.pathParameters.beacon;
-  const key = `beacon-${map_name}-${beacon_id}`;
+  const { map, beacon } = event.pathParameters;
+  const key = `beacon-${map}-${beacon}`;
 
   const result = await doc_client.get({ TableName: table_name, Key: { id: key } }).promise();
 
   if(result.Item)
     return { statusCode: 200, body: JSON.stringify(result.Item) };
   else
-    return { statusCode: 404, body: `Beacon ${beacon_id} doesn't exist for map ${map_name}` };
+    return { statusCode: 404, body: `Beacon ${beacon} doesn't exist for map ${map}` };
 }
