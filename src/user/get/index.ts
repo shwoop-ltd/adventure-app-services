a4c4946bfc927f5dcbd829a95af657664df653f8
Timothy Finucane <timfinucane@outlook.com>
2019-08-24 16:53:42 +1200

    Fix bugs and add event info

--- src/user/get/index.ts
+++ src/user/get/index.ts
@@ -3,8 +3,8 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { Users, response } from '/opt/nodejs';
 
 export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-  if(!event.pathParameters || !event.pathParameters.userid)
-    return response(400, "No userid");
+  if(!event.pathParameters || !event.pathParameters.user_id)
+    return response(400, "No user_id");
 
   const { user_id } = event.pathParameters;
 
