f95e95b2590e3101b9bff75a611b9eb7c71650b5
Nik Ponomarov <nosurlife@hotmail.com>
2020-04-15 20:03:56 +1200

    API no longer returns attributes 'beta', 'surveys', and 'treasure'

--- src/user/get-all/index.ts
+++ src/user/get-all/index.ts
@@ -5,11 +5,21 @@ export async function get_users(event: ApiRequest, model: Persistence): Promise<
   await model.telemetry.create('get-users');
 
   if (event.query && event.query.attributes) {
-    var users = await model.user.get_all(
-      Array.isArray(event.query.attributes) ? event.query.attributes : [event.query.attributes]
-    );
+    let attributes = Array.isArray(event.query.attributes) ? event.query.attributes : [event.query.attributes];
+    let sensitiveAttributes = ['beta', 'surveys', 'treasure'];
+    if (attributes.some((v) => sensitiveAttributes.indexOf(v) > -1)) {
+      return { code: 401, body: 'Cannot access requested attribute' };
+    }
+    var users = await model.user.get_all(attributes);
   } else {
-    var users = await model.user.get_all();
+    var users = await model.user.get_all([
+      'id',
+      'campaign',
+      'points',
+      'prizes',
+      'challenges',
+      'prerequisite_challenges_completed',
+    ]);
   }
 
   return { code: 200, body: users };
