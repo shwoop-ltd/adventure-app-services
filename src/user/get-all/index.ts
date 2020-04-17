949843167c83269a85b8ac13fa83d938c81bb3da
Nik Ponomarov <nosurlife@hotmail.com>
2020-04-17 17:47:10 +1200

    changed blacklist to whitelist in get_users endpoint

--- src/user/get-all/index.ts
+++ src/user/get-all/index.ts
@@ -4,22 +4,18 @@ import controller, { ApiResponse, ApiRequest } from '/opt/nodejs/controller';
 export async function get_users(event: ApiRequest, model: Persistence): Promise<ApiResponse> {
   await model.telemetry.create('get-users');
 
+  let attributesWhiteList = ['id', 'campaign', 'points', 'prizes', 'challenges', 'prerequisite_challenges_completed'];
+
   if (event.query && event.query.attributes) {
     let attributes = Array.isArray(event.query.attributes) ? event.query.attributes : [event.query.attributes];
-    let sensitiveAttributes = ['beta', 'surveys', 'treasure'];
-    if (attributes.some((v) => sensitiveAttributes.indexOf(v) > -1)) {
+
+    // refuses access to 'beta', 'surveys', 'treasure'
+    if (attributes.some((v) => attributesWhiteList.indexOf(v) == -1)) {
       return { code: 401, body: 'Cannot access requested attribute' };
     }
     var users = await model.user.get_all(attributes);
   } else {
-    var users = await model.user.get_all([
-      'id',
-      'campaign',
-      'points',
-      'prizes',
-      'challenges',
-      'prerequisite_challenges_completed',
-    ]);
+    var users = await model.user.get_all(attributesWhiteList);
   }
 
   return { code: 200, body: users };
