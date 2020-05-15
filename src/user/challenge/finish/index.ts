099b4d768cc8ae373b254db1879f39d3910686fd
Gus Porter <kiwiangus360@gmail.com>
2020-05-15 17:21:42 +1200

    Fixed thrown errors

--- src/user/challenge/finish/index.ts
+++ src/user/challenge/finish/index.ts
@@ -8,16 +8,12 @@ export async function finish_challenge(event: ApiRequest, model: Persistence): P
   }
 
   const body = JSON.parse(event.body);
-  if (!body.challenge_id || !body.location || !body.map) {
+  if (!body.challenge_id || !body.map) {
     return { code: 400, body: 'Incorrect body.' };
   }
 
   if (!event.path || !event.path.user_id) {
     return { code: 400, body: 'No user_id' };
-  }
-
-  if (!body.location.latitude || !body.location.longitude) {
-    return { code: 400, body: 'Invalid location format' };
   }
 
   // Done first to ensure our telemetry is about a given user.
@@ -35,16 +31,13 @@ export async function finish_challenge(event: ApiRequest, model: Persistence): P
   await model.telemetry.create('finish-challenge', user.id);
 
   // Core variable assignment
-  const { location, map, challenge_id } = body;
+  const { map, challenge_id } = body;
 
   const challenge = await model.challenge.get(map, challenge_id);
   if (!challenge) return { code: 404, body: 'Puzzle not found' };
 
   // Check the user hasn't already completed it
   if (user.challenges.includes(challenge_id)) return { code: 403, body: 'Challenge already completed.' };
-
-  // Solution Check, 50 meter radius
-  if (get_distance(challenge.solution, location) > 50) return { code: 204, body: 'Wrong solution.' };
 
   // Identify the prize that should be awarded.
   const prize_info = get_next_prize(challenge);
