fe90776988342b69f9a3ca8d1b8321fcba82e2b5
Carl Bennett <carlbennettnz@gmail.com>
2020-05-17 16:02:47 +1200

    Update build commands

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -3,10 +3,7 @@
 $stage = $args[0]
 $extras = $args | Select-Object -Skip 1
 
-# This is outdated
-# sam validate $extras
-
-tsc --p tsconfig.build.json
+yarn build
 
 sam package --template-file template.yaml --s3-bucket adventure-app-cloudformation --s3-prefix $stage --output-template-file cloudformation.yaml $extras
 sam deploy --template-file ./cloudformation.yaml --stack-name AdventureApp$stage --parameter-overrides Stage=$stage --capabilities CAPABILITY_IAM $extras
